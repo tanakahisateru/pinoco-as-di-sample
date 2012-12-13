ec32370405f2a6f231d8c5a9e3aefaff45ce8eb6
Hisateru Tanaka <tanakahisateru@gmail.com>
2012-12-14 01:36:37 +0900

    テストコードで不自然な依存がなくなることを表現

--- test/NewsletterTransferTest.php
+++ test/NewsletterTransferTest.php
@@ -5,6 +5,7 @@ class NewsletterTransferTest extends PHPUnit_Framework_TestCase
 
     function setUp()
     {
+        // 単体テストは本来ならこれが要らないはず
         $this->container = SystemDIContainerFactory::getContainer();
     }
 
@@ -14,7 +15,13 @@ class NewsletterTransferTest extends PHPUnit_Framework_TestCase
         $transfer->send("メリークリスマスだぜイェーイ");
     }
 
-    function testSend()
+    function testSendFromDIContainer()
+    {
+        $transfer = $this->container->transfer;
+        $transfer->send("メリークリスマス注入だぜイェーイ");
+    }
+
+    function testSendDependingDIContainer()
     {
         ob_start();
         $transfer = new NewsletterTransfer($this->container->testmailer);
@@ -24,9 +31,15 @@ class NewsletterTransferTest extends PHPUnit_Framework_TestCase
         $this->assertEquals("メリークリスマス注入だぜイェーイ\n", $result);
     }
 
-    function testSendViaDI()
+    function testSend()
     {
-        $transfer = $this->container->transfer;
+        // 本来のクリーンな単体テストはこう
+        ob_start();
+        $mailer = new EchoMailer; // テスト用のほう
+        $transfer = new NewsletterTransfer($mailer);
         $transfer->send("メリークリスマス注入だぜイェーイ");
+        $result = ob_get_clean();
+
+        $this->assertEquals("メリークリスマス注入だぜイェーイ\n", $result);
     }
 }
